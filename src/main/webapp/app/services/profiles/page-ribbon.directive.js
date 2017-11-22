d169961885e15279bdd55098c709d619199576bf
anisul <anis.2803@gmail.com>
2017-11-22 19:36:12 +0100

    badge removed, liquibase disabled

--- src/main/webapp/app/services/profiles/page-ribbon.directive.js
+++ src/main/webapp/app/services/profiles/page-ribbon.directive.js
@@ -11,7 +11,7 @@
         var directive = {
             replace : true,
             restrict : 'AE',
-            template : '<div class="ribbon hidden"><a href="" data-translate="global.ribbon.{{ribbonEnv}}">{{ribbonEnv}}</a></div>',
+            template : '',
             link : linkFunc
         };
 
